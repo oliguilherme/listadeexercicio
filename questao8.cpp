2de1dde7ec40c96251fd1edf4680a5d5d22a6f03
guilherme <92556199+guilhermejoji@users.noreply.github.com>
2022-03-13 16:09:46 -0300

    Update questao8.cpp

--- questao8.cpp
+++ questao8.cpp
@@ -6,10 +6,14 @@ int fatorial(int fat){
     int n;
     int result = 1;
 
-    for(n = fat; n >= 1; n--){
+    if(fat == 0){
+        return 1;
+    } else{
+        for(n = fat; n >= 1; n--){
         result = result * n;
     }
     return result;
+    }
 }
 
 int main(){
@@ -18,12 +22,12 @@ int main(){
     int valor, resposta;
 
     printf("\t\t\tCalculador de fatorial\n\n");
-    printf("Informe o número escolhido: ");
+    printf("Informe o nÃºmero escolhido: ");
     scanf("%d", &valor);
 
     resposta = fatorial(valor);
     
-    printf("O fatorial de %d! é: %d\n\n", valor, resposta);
+    printf("O fatorial de %d! Ã©: %d\n\n", valor, resposta);
 
     system("pause");
     return 0;
